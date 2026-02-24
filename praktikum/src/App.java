b60b4bbd5d9df1e2f965ef4d613bdbb14c081aa6
pauluss-arch <paulussiagian227@gmail.com>
2026-02-24 15:45:54 +0700

    latihan modul1

---
+++ praktikum/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Paulus Roberto Apolos Siagian");
+         System.out.println("105224020");
+    }
+}
